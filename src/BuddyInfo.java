96c00dae83f6ccdcbadb621eaeab2ce088c7c7e3
grahamaturner <grahamaturner@4377-017.ODENG2.CARLETON.CA>
2014-10-23 16:12:01 -0400

    New Classes

--- src/BuddyInfo.java
+++ src/BuddyInfo.java
@@ -1,7 +1,8 @@
 
 public class BuddyInfo {
 	
-	String name,address,phonenumber; 
+	String name,address,phonenumber,greeting;
+	int age;
 	
 	
 	public BuddyInfo(String name, String address, String phonenumber) {
@@ -9,23 +10,54 @@ public class BuddyInfo {
 		this.name = name;
 		this.address = address;
 		this.phonenumber = phonenumber;
+		this.age = -1;
+		this.greeting = "Depr3ci4ted";
 	}
 
+	public BuddyInfo(String name, String address, String phonenumber, int age, String greeting) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.phonenumber = phonenumber;
+		this.age = age;
+		this.greeting = greeting;
+	}
 
+	public BuddyInfo(BuddyInfo buddy){
+		this(buddy.getName(),buddy.getAddress(),buddy.getPhonenumber(),buddy.getAge(),buddy.getGreeting());
+	}
+	
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	public String getGreeting() {
+		return greeting;
+	}
 
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+
+	public boolean isOver18(int age) {
+		return (age>=18);
+	}
+	
 	public String getAddress() {
 		return address;
 	}
-
 
 	public void setAddress(String address) {
 		this.address = address;
@@ -35,7 +67,6 @@ public class BuddyInfo {
 	public String getPhonenumber() {
 		return phonenumber;
 	}
-
 
 	public void setPhonenumber(String phonenumber) {
 		this.phonenumber = phonenumber;
--- src/BuddyInfoTest.java
+++ src/BuddyInfoTest.java
@@ -1,9 +1,58 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 
 public class BuddyInfoTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@Before
+	public void setUp() throws Exception {
+	}
 
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testBuddyInfoBuddyInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGreeting() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetGreeting() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsOver18() {
+		fail("Not yet implemented");
 	}
 
 }
